3a24539199950be33412208b72071295b6b11ec5
Techi Rexach <techi90@gmail.com>
2021-06-01 20:05:39 +0200

    form and api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+
+  @api = StockQuote::Stock.new(api_key: 'pk_0d42640b3273439497d5bc858e3f60a7')
+
   def index
+    if params[:company]
+    @stock = StockQuote::Stock.quote(params[:company])
+    end
+    
   end
 
   def about
