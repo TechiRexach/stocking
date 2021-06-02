2d5a4968789ce9f6cb5ab80c9d570adb72ce6893
Techi Rexach <techi90@gmail.com>
2021-06-02 19:21:47 +0200

    error handling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,14 @@ class HomeController < ApplicationController
   @api = StockQuote::Stock.new(api_key: 'pk_0d42640b3273439497d5bc858e3f60a7')
 
   def index
-    if params[:company]
-    @stock = StockQuote::Stock.quote(params[:company])
+    if params[:company] == ''
+      @nothing = 'Hey, you forgot to enter a symbol!'
+    elsif params[:company]
+      @stock = StockQuote::Stock.quote(params[:company])
+      if !@stock 
+        @error = 'Hey, that stock symbol doesnt exist'
+      end
+      
     end
     
   end
