db7d7d7fa36f73d765d3cf5bf4dc38f9edb7dae1
Techi Rexach <techi90@gmail.com>
2021-06-03 12:29:00 +0200

    scaffold

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,14 @@ class HomeController < ApplicationController
   @api = StockQuote::Stock.new(api_key: 'pk_0d42640b3273439497d5bc858e3f60a7')
 
   def index
-    if params[:company] == ''
+    if params[:ticker] == ''
       @nothing = 'Hey, you forgot to enter a symbol!'
-    elsif params[:company]
-      @stock = StockQuote::Stock.quote(params[:company])
+    elsif params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
       if !@stock 
         @error = 'Hey, that stock symbol doesnt exist'
       end
     end
-    
   end
 
   def about
